9054bff1735524fbb14db3f1ae5265ea78268196
xmflsct <me@xmflsct.com>
2022-11-06 22:10:47 +0100

    Update for schema validation

--- packages/translate/src/middlewares/respond.ts
+++ packages/translate/src/middlewares/respond.ts
@@ -1,17 +1,12 @@
 import { Env, TheRequest } from '..'
 
-const respond = (
-  request: TheRequest,
-  _e: Env
-): Response => {
+const respond = (request: TheRequest, _e: Env): Response => {
   if (!request.outgoing) {
     throw new Error('Missing translation')
   }
 
   const response = new Response(JSON.stringify(request.outgoing), {
-    headers: {
-      'content-type': 'application/json;charset=UTF-8'
-    }
+    headers: { 'content-type': 'application/json' }
   })
   return response
 }
