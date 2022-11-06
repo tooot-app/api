2a68ddba177abe8d9565b906eddf04ad4dfa602c
xmflsct <me@xmflsct.com>
2022-11-06 21:33:39 +0100

    Add logging when subscribing failed

--- packages/push/src/routes/subscribe.ts
+++ packages/push/src/routes/subscribe.ts
@@ -1,11 +1,23 @@
-import { BodySubscribe, DurableObjectDevice, ParamsSubscribe } from '..'
+import { BodySubscribe, DurableObjectDevice, Env, ParamsSubscribe } from '..'
+import logToNR from '../utils/logToNR'
 
 const subscribe = async (
-  request: Request & DurableObjectDevice & ParamsSubscribe
+  request: Request & DurableObjectDevice & ParamsSubscribe,
+  env: Env,
+  context: ExecutionContext
 ): Promise<Response> => {
   const body = await request.json<BodySubscribe>()
 
   if (!body.accountFull || !body.serverKey) {
+    context.waitUntil(
+      logToNR(env.NEW_RELIC_KEY, {
+        tooot_push_log: 'error_subscribe_body',
+        workers_type: 'workers',
+        expoToken: request.params?.expoToken,
+        instanceUrl: request.params?.instanceUrl,
+        body
+      })
+    )
     return new Response('[subscribe] Data error', { status: 400 })
   }
 
