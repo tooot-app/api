b5b007823e6d4bff2327ef00100d4f5178bfc4ea
xmflsct <me@xmflsct.com>
2023-01-28 00:00:49 +0100

    Test connect service

---
+++ packages/connect/src/index.ts
@@ -0,0 +1,21 @@
+export default {
+  async fetch(
+    request: Request,
+    { ENVIRONMENT }: { ENVIRONMENT: 'development' | 'candidate' | 'release' }
+  ) {
+    const DOMAIN =
+      ENVIRONMENT === 'development'
+        ? 'tooot-connect-development.xmflsct.workers.dev'
+        : ENVIRONMENT === 'candidate'
+        ? 'connect-candidate.tooot.app'
+        : 'connect.tooot.app'
+
+    const headers = Object.fromEntries(request.headers)
+
+    return fetch(request.url.replace(DOMAIN, headers['x-tooot-domain']), {
+      method: request.method,
+      headers,
+      body: request.body
+    })
+  }
+}
